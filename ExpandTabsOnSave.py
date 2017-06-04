7422dc560cf76cd539f0c8d2f8daeabe5fef4acc
Mednikov Yuriy <myuriy@users.noreply.github.com>
2017-06-04 15:22:09 +0300

    Update ExpandTabsOnSave.py
    Check "convert_tabspaces_on_save" parameter for enabling 
    Add "set_translate_tabs" argument for expand_tabs / unexpand_tabs (ST3)

--- ExpandTabsOnSave.py
+++ ExpandTabsOnSave.py
@@ -1,7 +1,15 @@
-import sublime, sublime_plugin, os
+# coding: utf-8
+""" ExpandTabsOnSave SublimeText3 plugin """
+import sublime_plugin
+
 
 class ExpandTabsOnSave(sublime_plugin.EventListener):
-  # Run ST's 'expand_tabs' command when saving a file
-  def on_pre_save(self, view):
-    if view.settings().get('expand_tabs_on_save') == 1:
-      view.window().run_command('expand_tabs')
+    """Expand tabs / spaces on file save."""
+
+    def on_pre_save(self, view):
+        """ Run ST's 'expand_tabs' command when saving a file. """
+        if view.settings().get('convert_tabspaces_on_save'):
+            if view.settings().get('translate_tabs_to_spaces'):
+                view.run_command('expand_tabs', {"set_translate_tabs": True})
+            else:
+                view.run_command('unexpand_tabs', {"set_translate_tabs": True})
